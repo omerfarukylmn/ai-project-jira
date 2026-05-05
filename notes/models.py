dbfc16b94a6798febb2a0e6026f4b92ce1f79271
emirhan <116457536+emirhanerdem@users.noreply.github.com>
2026-05-05 22:42:26 +0300

    Update __str__ method comment in models.py

--- notes/models.py
+++ notes/models.py
@@ -8,5 +8,5 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self): ##edit
+    def __str__(self): ##return
         return self.title
